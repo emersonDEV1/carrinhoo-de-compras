3b5d8240e53150236cdfd6b68639ed0e57c50e72
emersonDEV1 <144362487+emersonDEV1@users.noreply.github.com>
2023-10-30 17:50:45 -0300

    subir codigo

---
+++ pizzas.js
@@ -0,0 +1,9 @@
+let pizzaJson = [
+    {id:1, name:'Salada Primavera', img:'images/salada-1.png', price:22.00, sizes:['100g', '530g', '860g'], description:'Tomate, Pão, Azeitonas, Manjericão.'},
+    {id:2, name:'Salada Green', img:'images/salada-2.png', price:16.00, sizes:['320g', '530g', '860g'], description:'Tomate, cebola, alface e manjericao'},
+    {id:3, name:'Salada Hot', img:'images/salada-3.png', price:16.00, sizes:['320g', '530g', '860g'], description:'Alface De Cordeiro Rúcula fresca.'},
+    {id:4, name:'Salada Fit', img:'images/salada-4.png', price:18.50, sizes:['320g', '530g', '860g'], description:'Alface, Tomate, Cenoura, Cebola.'},
+    {id:5, name:'Suco de abacaxi', img:'images/suco-1.png', price:4.50, sizes:['100ml', '100ml', '100ml'], description:'Favorece o rendimento físico.'},
+    {id:6, name:'Suco de laranja', img:'images/suco-2.png', price:5.00, sizes:['250ml', '250ml', '250ml'], description:'Saúde do Coração.'},
+    {id:7, name:'Vitamina de abacate', img:'images/suco-3.png', price:7.50, sizes:['400ml', '400ml', '400ml'], description:'Favorece o ganho de massa muscular.'}
+];
